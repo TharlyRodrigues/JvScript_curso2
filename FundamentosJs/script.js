bc9df39b07963619df81a4dc515d0828031afdb3
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-06-14 17:41:49 -0300

    data types

--- FundamentosJs/script.js
+++ FundamentosJs/script.js
@@ -1,1 +1,34 @@
+/*
 // console.log(100);
+
+let primeiroNome = "tharly";
+let segundoNome = "Rodrigues";
+
+console.log(primeiroNome, segundoNome);
+
+let ano = "1995";
+
+console.log(ano);
+*/
+
+// strings
+
+const PrimeiroNome = "Tharly";
+
+// number
+
+const idade = 29;
+const ano = 1995;
+
+//Boolean
+const hasKids = true;
+
+// undefined
+// let score;
+const score = undefined;
+
+// Null
+const aptNumber = null;
+const output = aptNumber;
+
+console.log(PrimeiroNome, typeof PrimeiroNome);
