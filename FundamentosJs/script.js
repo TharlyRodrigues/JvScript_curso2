abb8e2a9482b2726351a3644e1c3b2f6316bfd5d
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-06-15 18:52:17 -0300

    type conversion

--- FundamentosJs/script.js
+++ FundamentosJs/script.js
@@ -9,7 +9,7 @@ console.log(primeiroNome, segundoNome);
 let ano = "1995";
 
 console.log(ano);
-*/
+
 
 // strings
 
@@ -32,3 +32,50 @@ const aptNumber = null;
 const output = aptNumber;
 
 console.log(PrimeiroNome, typeof PrimeiroNome);
+
+
+// value are stored on the stack
+
+const name = "tharly";
+const age = "29";
+
+// reference value are stored on the heap
+
+const person = {
+  name: "tharly",
+  age: "29",
+};
+
+let newName = name;
+newName = "Johnathan";
+
+let newPerson = person;
+newPerson.name = "bradley";
+console.log(person, newPerson);
+console.log(name, newName);
+
+
+// type conversion
+
+let amount = "99.5";
+
+// change string to number
+// amount = parseInt(amount);
+// amount = Number(amount);
+// amount = +amount;
+
+// change number to string
+
+// amount = amount.toString();
+// amount = String(amount);
+
+// change string to decimal
+
+// amount = parseInt(amount);
+
+// change number to boolean
+
+amount = Boolean(amount);
+
+console.log(amount, typeof amount);
+*/
