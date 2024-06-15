5a92af6b4f4c4a7bb2edd2808e51979cc83ec0c4
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-06-15 19:20:36 -0300

    operators

--- FundamentosJs/script.js
+++ FundamentosJs/script.js
@@ -78,4 +78,51 @@ let amount = "99.5";
 amount = Boolean(amount);
 
 console.log(amount, typeof amount);
+
+
+// operators
+
+let x;
+x = 5 + 5;
+x = 5 - 5;
+x = 5 / 5;
+x = 5 * 5;
+x = 7 % 5;
+
+// concatenation
+x = "hello" + " " + "wold";
+
+// exponent
+
+// x = 2 ** 3;
+
+// increment
+x = 1;
+x++;
+
+// decrement
+// x = x - 1;
+x--;
+
+// assignment operators
+
+// x = 10;
+// x -= 5;
+// x *= 5;
+// x /= 5;
+// x %= 5;
+x **= 5;
 */
+
+// comparison operators
+x = 2 == 2;
+x = 2 === "2";
+x = 2 != "2";
+x = 2 !== 2;
+
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 5;
+
+console.log(x);
