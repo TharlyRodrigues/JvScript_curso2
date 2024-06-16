3102df861ebde9502d8acb1d4b680c02ba7d32cb
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-06-16 06:07:58 -0300

    type coercion

--- FundamentosJs/script.js
+++ FundamentosJs/script.js
@@ -112,7 +112,7 @@ x--;
 // x /= 5;
 // x %= 5;
 x **= 5;
-*/
+
 
 // comparison operators
 x = 2 == 2;
@@ -126,3 +126,25 @@ x = 10 <= 5;
 x = 10 >= 5;
 
 console.log(x);
+*/
+
+// type coercion
+
+let x;
+x = 5 + "5";
+x = 5 + Number("5");
+
+x = 5 * "5";
+
+x = 5 + null;
+
+x = Number(null);
+x = Number(true);
+x = Number(false);
+
+x = 5 + true;
+x = 5 + false;
+
+x = 5 + undefined;
+
+console.log(x, typeof x);
