1f283ff86b1eea41a65a74bcd88115aa3c7ab487
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-06-20 18:57:12 -0300

    array

--- FundamentosJs/script.js
+++ FundamentosJs/script.js
@@ -212,3 +212,26 @@ myNewString = `${myString[0].toUpperCase()}${myString.substring(1)}`;
 
 console.log(myNewString);
 */
+
+const arr = [1, 2, 3, 4, 5];
+
+arr.reverse();
+arr.push(0);
+arr.unshift(6);
+console.log(arr);
+
+// challenge 2
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 6, 7, 8, 9, 10];
+
+// solução 1
+
+const arr3 = arr1.slice(0, 4).concat(arr2);
+
+// solução
+const arr4 = [...arr1, ...arr2];
+arr4.splice(4, 1);
+arr4.push(11);
+arr4.reverse();
+console.log(arr4);
