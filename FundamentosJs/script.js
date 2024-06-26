1ed8060f0be56673e81b2401a3b0e75910b36a71
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-06-26 17:28:08 -0300

    Operador e métodos de propagação de objetos

--- FundamentosJs/script.js
+++ FundamentosJs/script.js
@@ -512,7 +512,6 @@ const arr4 = [...arr1, ...arr2];
 arr4.splice(4, 1);
 console.log(arr4);
 
-*/
 let x;
 
 const person = {
@@ -546,3 +545,52 @@ person.greet = function () {
   console.log(`hello, my name is ${this.name}`);
 };
 console.log(x);
+
+
+let x;
+
+const todo = new Object();
+
+todo.id = 1;
+todo.name = "tharly rodrigues";
+todo.completed = false;
+
+x = todo;
+
+const person = {
+  address: {
+    coords: {
+      lat: 42.9384,
+      lng: -71.3232,
+    },
+  },
+};
+
+x = person.address.coords.lat;
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { c: 3, d: 4 };
+
+// const obj3 = { obj1, obj2 };
+// const obj3 = { ...obj1, ...obj2 };
+const obj4 = Object.assign({}, obj1, obj2);
+
+const todos = [
+  { id: 1, name: "buy MIlk" },
+  { id: 2, name: "Thiago MIlk" },
+  { id: 3, name: "Tharly MIlk" },
+];
+
+x = todos[0].name;
+
+x = Object.keys(todo);
+x = Object.keys(todo).length;
+
+x = Object.values(todo);
+
+x = Object.entries(todo);
+
+x = todo.hasOwnProperty("age");
+
+console.log(x);
+*/
