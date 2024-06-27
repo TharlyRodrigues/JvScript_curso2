c2caf658a0743a1c448d19923b95c68b19e2d6e0
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-06-27 18:17:14 -0300

    parâmetros e argumentos

--- FundamentosJs/script.js
+++ FundamentosJs/script.js
@@ -707,7 +707,7 @@ console.log(firstBook);
 
 const libraryJson = JSON.stringify(library);
 console.log(libraryJson);
-*/
+
 
 function sayHello() {
   console.log("hello world");
@@ -724,14 +724,73 @@ sumNumbers(5, 10);
 function subtract(num1, num2) {
   return num1 - num2;
 
-  console.log("after the return");
+  // console.log("after the return");
 }
 
 const result = subtract(10, 2);
 console.log(result, subtract(15, 8));
 
-function registerUser(user) {
+function registerUser(user = "bot ") {
   return user + "is registered";
 }
 
-console.log(registerUser("tharly Rodrigues"));
+console.log(registerUser());
+
+function sum2(...Numbers) {
+  return Numbers;
+}
+console.log(sum2(1, 2, 3));
+
+function sum(...Numbers) {
+  let total = 0;
+
+  for (const num of Numbers) {
+    total += num;
+  }
+
+  return total;
+}
+console.log(sum(1, 2, 3, 4, 5));
+
+function sum(...Numbers2) {
+  let total2 = 0;
+
+  for (const num of Numbers2) {
+    total2 += num;
+  }
+
+  return total2;
+}
+
+console.log(sum(1, 2));
+
+// objects as params
+
+function loginUser(user) {
+  return `The user ${user.name} with the id of ${user.id} is logged in`;
+}
+
+const user = {
+  id: 1,
+  name: "tharly rodrigues",
+};
+
+console.log(loginUser(user));
+console.log(
+  loginUser({
+    id: 2,
+    name: "Thiago rodrigues",
+  })
+);
+
+// array params
+function getRandom(...arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+
+  const item = arr[randomIndex];
+
+  console.log(item);
+}
+
+getRandom(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+*/
