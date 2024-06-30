3167896fa796f3661e7d07ef70c8078252f5f458
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-06-30 19:36:08 -0300

    entendendo funções

--- FundamentosJs/script.js
+++ FundamentosJs/script.js
@@ -978,3 +978,16 @@ console.log(minMax([1, 2, 3, 4, 5, 6]));
 })(10, 5);
 
 */
+
+// entendo funções
+const x = 100;
+const y = 50;
+
+function getSum(n1, n2) {
+  const sum = n1 + n2;
+  return sum;
+}
+
+const sum1 = getSum(x, y);
+const sum2 = getSum(10, 5);
+console.log(sum1, sum2);
