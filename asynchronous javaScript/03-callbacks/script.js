7421e2071fddf1e93f32450917379beadff536eb
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-08-06 20:34:37 -0300

    Retornos de chamada

--- asynchronous javaScript/03-callbacks/script.js
+++ asynchronous javaScript/03-callbacks/script.js
@@ -1,6 +1,46 @@
-function toggle(e) {
-  console.log('callback ran');
-  e.target.classList.toggle('danger');
+// function toggle(e) {
+//   e.target.classList.toggle("danger");
+// }
+// document.querySelector("button").addEventListener("click", toggle);
+
+const posts = [
+  { title: "post one", body: "thi is post one" },
+  { title: "post two", body: "thi is post two" },
+];
+
+// function creatPost(post, cb) {
+//   setTimeout(() => {
+//     posts.push(post);
+//     cb();
+//   }, 2000);
+// }
+
+// function getPosts() {
+//   setTimeout(() => {
+//     posts.forEach(function (post) {
+//       const div = document.createElement("div");
+//       div.innerHTML = `<strong>${post.title}</strong> - ${post.body}`;
+//       document.querySelector("#posts").appendChild(div);
+//     });
+//   }, 1000);
+// }
+
+// creatPost({ title: "post Three", body: "thi is post three" }, getPosts);
+
+function creatPost(post, cb) {
+  setTimeout(() => {
+    posts.push(post);
+    cb();
+  }, 2000);
+}
+function getPosts1() {
+  setTimeout(() => {
+    posts.forEach(function (post) {
+      const div = document.createElement("div");
+      div.innerHTML = `<strong>${post.title}</strong> - ${post.body}`;
+      document.querySelector("#posts").appendChild(div);
+    });
+  }, 1000);
 }
 
-document.querySelector('button').addEventListener('click', toggle);
+creatPost({ title: "post three", body: "thi is post three" }, getPosts1);
